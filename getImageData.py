4d866ab65495bf967aee402894ba590ed9db17f2
gospe <gospex@gmail.com>
2016-01-12 22:26:40 -0500

    Progress

--- getImageData.py
+++ getImageData.py
@@ -37,8 +37,21 @@ def getPatches():
     print(images.shape)
     return images
 
+def getPatches(n):
+    images = np.array(scipy.io.loadmat("IMAGES.mat")["IMAGES"])
+    NUM_PICS = images.shape[2]
+    PATCH_SIZE = 8
+    IMAGE_SIZE = images.shape[0]
+
+    images = np.array([[images[j:j+PATCH_SIZE,k:k+PATCH_SIZE, i].ravel() for i,j,k in getrands(NUM_PICS,PATCH_SIZE,IMAGE_SIZE,n) ]])
+    return images
 
 
+def getrands(NUM_PICS, PATCH_SIZE, IMAGE_SIZE, num_rands):
+    for x in range(num_rands):
+        yield rand.randint(0,NUM_PICS-1),rand.randint(0,IMAGE_SIZE-PATCH_SIZE-1),rand.randint(0,IMAGE_SIZE-PATCH_SIZE-1)
 
 
-
+for row in getPatches(1):
+    print(row)
+    print(row.T)
