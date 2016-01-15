8370b0be7e0514e3082d684368d8185dfc2f08d6
gospe <gospex@gmail.com>
2016-01-14 23:19:02 -0500

    Progress

--- getImageData.py
+++ getImageData.py
@@ -6,52 +6,30 @@ __author__ = 'JEFFERYK'
 def normalizeData(myData):
 
     #Remove the Mean
-    avg = np.mean(myData.ravel())
-    print("Average: ",avg)
+    avg = np.mean(myData,axis=0)
     myData -= avg
 
     #TRUNCATE to 3 sigmas
     std = np.std(myData)
-    print("STD: ",std)
     std *= 3
     myData = np.maximum(np.minimum(myData, std),-1*std)/std
 
     #Rescale  from [-1,1] to [0.1,0.9]
     myData = (myData + 1) * 0.4 + 0.1
 
-    print("New Average: ", np.mean(myData))
-    print("New STD: ", np.std(myData))
-
     return myData
-
-
-
-def getPatches():
-    #Load Images.mat
-    images = np.array(scipy.io.loadmat("IMAGES.mat")["IMAGES"])
-    NUM_PICS = images.shape[2]
-    PATCH_SIZE = 8
-    IMAGE_SIZE = images.shape[0]
-    # Unroll Images in PATCH_SIZE^2 long vectors
-    images = np.array([[images[j:j+PATCH_SIZE,k:k+PATCH_SIZE, i].ravel() for i in range(NUM_PICS) for j in range(0, IMAGE_SIZE, PATCH_SIZE) for k in range(0, IMAGE_SIZE, PATCH_SIZE)]])
-    print(images.shape)
-    return images
 
 def getPatches(n):
     images = np.array(scipy.io.loadmat("IMAGES.mat")["IMAGES"])
     NUM_PICS = images.shape[2]
     PATCH_SIZE = 8
     IMAGE_SIZE = images.shape[0]
-
-    images = np.array([[images[j:j+PATCH_SIZE,k:k+PATCH_SIZE, i].ravel() for i,j,k in getrands(NUM_PICS,PATCH_SIZE,IMAGE_SIZE,n) ]])
-    return images
-
-
-def getrands(NUM_PICS, PATCH_SIZE, IMAGE_SIZE, num_rands):
-    for x in range(num_rands):
-        yield rand.randint(0,NUM_PICS-1),rand.randint(0,IMAGE_SIZE-PATCH_SIZE-1),rand.randint(0,IMAGE_SIZE-PATCH_SIZE-1)
+    patches = np.zeros((n,PATCH_SIZE*PATCH_SIZE))
+    for i in range(n):
+        x,y,z = getrands(NUM_PICS,PATCH_SIZE,IMAGE_SIZE)
+        patches[i,:] = images[y:y+PATCH_SIZE,z:z+PATCH_SIZE, x].ravel()
+    return normalizeData(patches)
 
 
-for row in getPatches(1):
-    print(row)
-    print(row.T)
+def getrands(NUM_PICS, PATCH_SIZE, IMAGE_SIZE):
+    return rand.randint(0,NUM_PICS-1),rand.randint(0,IMAGE_SIZE-PATCH_SIZE-1),rand.randint(0,IMAGE_SIZE-PATCH_SIZE-1)
--- testTensorFlowSparceAutoencoder.py
+++ testTensorFlowSparceAutoencoder.py
@@ -6,15 +6,16 @@ import tensorflow as tf
 __author__ = 'JEFFERYK'
 
 #Network Parameters
-n_epoch_size = 1000
-n_num_epochs = 10
+n_epoch_size = 10000
+n_num_epochs = 1
 n_input = 64
 n_hidden = 25
 n_output = n_input
-n_lambda = 0.0001
-n_rho = 0.01
-n_beta = 3
-learning_rate = .01
+n_lambda = tf.constant(0.0001)
+n_rho = tf.constant(0.01)
+n_beta = tf.constant(3)
+learning_rate = tf.constant(.01)
+ONE = tf.constant(1.0)
 
 x = tf.placeholder("float", [None, n_input])
 hidden = tf.placeholder("float", [None, n_hidden])
@@ -37,17 +38,20 @@ weights = {
 }
 
 biases = {
-    'hidden': tf.Variable(tf.random_normal([n_input, n_hidden])),
-    'out': tf.Variable(tf.random_normal([n_hidden, n_output]))
+    'hidden': tf.Variable(tf.random_normal([n_hidden])),
+    'out': tf.Variable(tf.random_normal([n_output]))
 }
 
 #Construct my model
 pred = autoencoder(x, weights, biases)
 
-rho_hat = tf.reduce_mean(pred['hidden'])#fix this
-sparce_cost = tf.reduce_sum( tf.add(tf.mul(n_rho , tf.log(tf.div(n_rho,x))) , tf.mul(tf.sub(1 , n_rho), tf.log(tf.div(tf.sub(1,n_rho),tf.sub(1,x))))))
+rho_hat = tf.reduce_mean(pred['hidden'],1)
+
 #Construct cost
-cost = tf.add(tf.add(tf.reduce_mean(tf.nn.l2_loss(pred-x)) , tf.mul(n_lambda,tf.nn.l2_loss(weights.values()))) , sparce_cost)
+cost_sparce = tf.reduce_sum(tf.add(tf.mul(n_rho, tf.log(tf.div(n_rho, x))), tf.mul(tf.sub(ONE, n_rho), tf.log(tf.div(tf.sub(ONE, n_rho), tf.sub(ONE, x))))))
+cost_J = tf.reduce_mean(tf.nn.l2_loss(pred['out']-x))
+cost_reg = tf.mul(n_lambda,tf.add(tf.nn.l2_loss(weights['hidden']),tf.nn.l2_loss(weights['out'])))
+cost = tf.add(tf.add(cost_J , cost_reg ), cost_sparce)
 
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
@@ -60,7 +64,7 @@ init = tf.initialize_all_variables()
 # Launch the graph
 with tf.Session() as sess:
     sess.run(init)
-    saver = tf.train.Saver({'weights':weights, 'biases':biases} )
+    saver = tf.train.Saver()
 
     # Training cycle
     for epoch in range(n_num_epochs):
